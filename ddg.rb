0ffc9e7490282b47bcb0a1a4806d0e57b54a5424
David Deller <david@horizon-nigh.org>
2011-11-28 13:45:48 -0500

    Make the code style a bit more 'Ruby-like' by removing extra lines where I expect braces, etc. to go

--- ddg.rb
+++ ddg.rb
@@ -15,26 +15,16 @@ class DDGSafariSetup
   
   
   def initialize(command)
-  
     if (command == 'install') then
-    
       install
-      
     elsif (command == 'uninstall') then
-    
       uninstall
-      
     else
-    
       welcome
-      
     end
-  
   end
   
-  
   def welcome
-    
     header = 'DuckDuckGo Safari Setup: READ CAREFULLY'
     info = "This app will replace the Yahoo search in your Safari browser with DuckDuckGo. Your administrator password will be required.
     
@@ -47,26 +37,17 @@ For wizards only: This is just going to add an entry to your
     button_clicked = `#{COCOA_DIALOG} msgbox --button1 Quit --button2 Install --button3 Uninstall --text "#{header}" --informative-text "#{info}"`.to_i
     
     if (button_clicked == 2) then
-      
       execAsAdmin('install')
-      
     elsif (button_clicked == 3) then
-    
       execAsAdmin('uninstall')
-    
     else
-    
       exit
-      
     end
-    
   end
   
   
   def install
-    
     begin
-      
       # clean up previous installations
       doUninstall()
       
@@ -75,105 +56,73 @@ For wizards only: This is just going to add an entry to your
       info = 'Installation successful.'
       detail = 'Please quit and reopen Safari for changes to take effect.'
       `#{COCOA_DIALOG} msgbox --button1 OK --text "#{info}" --informative-text "#{detail}"`
-      
     rescue
-    
       info = 'Installation failed.'
       detail = 'The file /etc/hosts could not be accessed.'
       `#{COCOA_DIALOG} msgbox --button1 OK --text "#{info}" --informative-text "#{detail}"`
-    
     end
-    
   end
   
-  
   def doInstall
-  
     file = File.new(HOSTS_FILE, 'a+')
     file << "\n"
     file << "#{HOSTS_LINE}\n"
     file.close
-  
   end
   
-  
   def uninstall
-  
     begin
-      
       matches = doUninstall()
       
       if (matches > 0) then
-        
         info = 'Uninstallation successful.'
         detail = 'Please quit and reopen Safari for changes to take effect.'
         `#{COCOA_DIALOG} msgbox --button1 OK --text "#{info}" --informative-text "#{detail}"`
-      
       else
-      
         info = 'There was nothing to uninstall.'
         `#{COCOA_DIALOG} msgbox --button1 OK --text "#{info}"`
-      
       end
-      
     rescue
-    
       info = 'Uninstallation failed.'
       detail = 'The file /etc/hosts could not be accessed.'
       `#{COCOA_DIALOG} msgbox --button1 OK --text "#{info}" --informative-text "#{detail}"`
-    
     end
-  
   end
   
   
   def doUninstall
-  
     new_lines = []
     matches = 0
     
     file = File.new(HOSTS_FILE, 'r')
     
     file.each do |line|
-    
       if (line.match(/#{HOSTS_TAG}/)) then
-      
         matches += 1
-        
       else
-      
         new_lines.push(line)
-      
       end
-    
     end
     
     if (matches > 0) then
-    
       file = File.new(HOSTS_FILE, 'w')
       
       new_lines.each do |line|
-      
         file << line
-      
       end
       
       file.close
-    
     end
     
     return matches
-  
   end
   
   
   def execAsAdmin(command)
-    
     shell_script = "'" + $0 + "' " + command
     applescript = 'do shell script \"' + shell_script + '\" with administrator privileges'
   
     result = `osascript -e "#{applescript}"`
-  
   end
 
 end
